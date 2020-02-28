1504d443d7b979f1d746d45239b5078a824c69fc
Alessio G. Baroni <abaroni@redhat.com>
2020-02-28 16:27:50 +0100

    Added other API

--- app.py
+++ app.py
@@ -3,9 +3,18 @@ import json
 
 app = flask.Flask(__name__)
 
+class User:
+    def __init__(self, name):
+        self.name = name
+
 @app.route('/')
 def index():
-    return flask.Response(json.dumps({'name': 'Alessio'}), mimetype = 'application/json')
+    return flask.Response(json.dumps(User('Alessio').__dict__),
+                          mimetype = 'application/json')
+
+@app.route('/hello/<name>')
+def hello(name):
+    return f'Ciao {name}'
 
 if __name__ == '__main__':
     app.run(host = '0.0.0.0', port = 8080)
